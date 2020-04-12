fcff1a9d18f498decb9d4f883d7c09016442c645
SzymonSmykala <ctk7000@gmail.com>
2020-04-12 10:10:58 +0200

    added BasketItemsController

--- exercise-2/app/controllers/BasketItemsController.scala
+++ exercise-2/app/controllers/BasketItemsController.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import DTO.Product
+import DTO.{BasketItem}
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
 import play.api.libs.json._
@@ -10,18 +10,18 @@ import scala.collection.mutable.ListBuffer
 
 
 @Singleton
-class ProductController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class BasketItemsController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def readAll = Action {
     var result: ListBuffer[Product] = ListBuffer();
     for (i <- 0 to 10){
-        result += Product(s"Product$i ", i + 1, i);
+      result += BasketItem(s"Product$i ", i + 1, i);
     }
     Ok(Json.toJson(result.toList))
   }
 
   def get(id: Long) = Action {
-    var result = Product("name", 1, 1)
+    var result = BasketItem("name", 1, 1)
     Ok(Json.toJson(result));
   }
 
@@ -33,10 +33,10 @@ class ProductController @Inject()(cc: ControllerComponents) extends AbstractCont
   }
 
   def add() = Action { request =>
-      val json = request.body.asJson.get
-      val product = json.as[Product]
-      print(product)
-      Ok(Json.toJson(product))
+    val json = request.body.asJson.get
+    val product = json.as[BasketItem]
+    print(product)
+    Ok(Json.toJson(product))
   }
 
   def delete(id: Long) = Action{
