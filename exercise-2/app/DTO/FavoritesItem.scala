08f4f336922d0686799c305a49a847ab9167859e
SzymonSmykala <ctk7000@gmail.com>
2020-04-12 14:20:23 +0200

    added FavoriteItem DTO

---
+++ exercise-2/app/DTO/FavoritesItem.scala
@@ -0,0 +1,9 @@
+package DTO
+
+import play.api.libs.json.Json
+
+case class FavoriteItem(var id: Float, var userId: Float, var productId: Float)
+
+object FavoriteItem{
+  implicit val favoriteItemFormat = Json.format[FavoriteItem]
+}
