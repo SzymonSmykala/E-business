16fba495ebd19a0f811a09bfa2aa44907d0d3cb6
SzymonSmykala <ctk7000@gmail.com>
2020-04-12 11:34:32 +0200

    added BasketItemsController

--- exercise-2/app/controllers/BasketItemsController.scala
+++ exercise-2/app/controllers/BasketItemsController.scala
@@ -13,21 +13,21 @@ import scala.collection.mutable.ListBuffer
 class BasketItemsController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def readAll = Action {
-    var result: ListBuffer[Product] = ListBuffer();
+    var result: ListBuffer[BasketItem] = ListBuffer();
     for (i <- 0 to 10){
-      result += BasketItem(s"Product$i ", i + 1, i);
+      result += BasketItem(i, 1,2,3);
     }
     Ok(Json.toJson(result.toList))
   }
 
   def get(id: Long) = Action {
-    var result = BasketItem("name", 1, 1)
+    var result = BasketItem(1, 1, 1,1)
     Ok(Json.toJson(result));
   }
 
   def update() = Action { request =>
     val json = request.body.asJson.get
-    val product = json.as[Product]
+    val product = json.as[BasketItem]
     print(product)
     Ok(Json.toJson(product))
   }
