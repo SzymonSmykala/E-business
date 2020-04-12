2be3256ae885780ae98a375c9a90a47c8765d99b
SzymonSmykala <ctk7000@gmail.com>
2020-04-12 15:12:23 +0200

    added FavoriteItemsController

---
+++ exercise-2/app/controllers/FavoriteItemsController.scala
@@ -0,0 +1,45 @@
+package controllers
+
+import DTO.FavoriteItem
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+import play.api.libs.json._
+import javax.inject._
+
+import scala.collection.mutable.ListBuffer
+
+
+@Singleton
+class FavoriteItemsController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  def readAll = Action {
+    var result: ListBuffer[FavoriteItem] = ListBuffer();
+    for (i <- 0 to 10){
+      result += FavoriteItem(i, 1, 2);
+    }
+    Ok(Json.toJson(result.toList))
+  }
+
+  def get(id: Long) = Action {
+    var result = FavoriteItem(1, 1,2)
+    Ok(Json.toJson(result));
+  }
+
+  def update() = Action { request =>
+    val json = request.body.asJson.get
+    val order = json.as[FavoriteItem]
+    print(order)
+    Ok(Json.toJson(order))
+  }
+
+  def add() = Action { request =>
+    val json = request.body.asJson.get
+    val order = json.as[FavoriteItem]
+    print(order)
+    Ok(Json.toJson(order))
+  }
+
+  def delete(id: Long) = Action{
+    Ok(Json.toJson("completed"))
+  }
+}
