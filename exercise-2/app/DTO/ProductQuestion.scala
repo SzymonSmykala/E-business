eb23cbece3015319be280e95ac01a53fd98d9d9d
SzymonSmykala <ctk7000@gmail.com>
2020-04-12 14:30:12 +0200

    added ProductQuestionEntity

---
+++ exercise-2/app/DTO/ProductQuestion.scala
@@ -0,0 +1,9 @@
+package DTO
+
+import play.api.libs.json.Json
+
+case class ProductQuestion(var id: Float, var productId: Float, var questionContent: String)
+
+object ProductQuestion{
+  implicit val productQuestionFormat = Json.format[ProductQuestion]
+}
