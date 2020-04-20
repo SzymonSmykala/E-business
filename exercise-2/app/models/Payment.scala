71306459b7bc88560eeb5b53d5f123b1902e68aa
SzymonSmykala <ctk7000@gmail.com>
2020-04-20 18:55:05 +0200

    added payment

--- exercise-2/app/models/Payment.scala
+++ exercise-2/app/models/Payment.scala
@@ -2,7 +2,7 @@ package models
 
 import play.api.libs.json.Json
 
-case class Payment (var id: Float, var status: String)
+case class Payment (var id: Long, var status: String)
 
 object Payment{
   implicit val paymentFormat = Json.format[Payment]
---
+++ exercise-2/app/models/PaymentRepository.scala
@@ -0,0 +1,48 @@
+package models
+
+import javax.inject.Inject
+import play.api.db.slick.DatabaseConfigProvider
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class PaymentRepository @Inject() (dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext){
+  val dbConfig = dbConfigProvider.get[JdbcProfile]
+
+  import dbConfig._
+  import profile.api._
+
+  private val payment = TableQuery[PaymentTable]
+  private class PaymentTable(tag: Tag) extends Table[Payment](tag, "payment") {
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+    def status = column[String]("status")
+    def * = (id, status) <> ((Payment.apply _).tupled, Payment.unapply)
+  }
+
+  def create(id: Long, status: String): Future[Payment] = db.run {
+    (payment.map(c => (c.status))
+      returning payment.map(_.id)
+      into {case ((status), id) => Payment(id, status)}
+      ) += (status)
+  }
+
+  def list(): Future[Seq[Payment]] = db.run {
+    payment.result
+  }
+
+  def getById(id: Long): Future[Payment] = db.run{
+    payment.filter(_.id === id).result.head
+  }
+
+  def delete(id: Long): Future[Int] = db.run {
+    payment.filter(_.id === id).delete
+  }
+
+  def update(id: Long, newPayment: Payment): Future[Unit] = {
+    val productToUpdate: Payment = newPayment.copy(id, newPayment.status)
+    db.run {
+      payment.filter(_.id === id).update(productToUpdate).map(_ => ())
+    }
+  }
+
+}
