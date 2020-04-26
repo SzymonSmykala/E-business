28fc2b30e99d63ea6431bdeed0cb8d764a41bd48
SzymonSmykala <ctk7000@gmail.com>
2020-04-26 16:10:49 +0200

    promo forms ready

--- exercise-2/app/controllers/PromoController.scala
+++ exercise-2/app/controllers/PromoController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import javax.inject.{Inject, _}
-import models.{Product, ProductRepository, Promo, PromoRepository}
+import models.{Category, Product, ProductRepository, Promo, PromoRepository}
 import play.api.data.Form
 import play.api.data.Forms.{mapping, _}
 import play.api.libs.json._
@@ -36,6 +36,42 @@ class PromoController @Inject()(cc: MessagesControllerComponents, promoRepositor
     }
   }
 
+  def updatePromoForm(id: Long): Action[AnyContent] = Action.async { implicit request: MessagesRequest[AnyContent] =>
+    var products:Seq[Product] = Seq[Product]()
+    productRepository.list().onComplete{
+      case Success(cat) => products = cat
+      case Failure(_) => print("fail")
+    }
+
+    val promo = promoRepository.getById(id)
+    promo.map(p => {
+      val prodForm = promoUpdateForm.fill(UpdatePromoForm(p.id.toInt, p.productId.toInt, p.looseAmount))
+      Ok(views.html.promoupdate(prodForm, products))
+    })
+
+  }
+
+  def updatePromoHandle = Action.async { implicit  request =>
+    var prod:Seq[Product] = Seq[Product]()
+    productRepository.list().onComplete{
+      case Success(value) => prod = value
+      case Failure(_) => print("fail")
+    }
+    promoUpdateForm.bindFromRequest.fold(
+      errorForm => {
+        Future.successful(
+          BadRequest("Bad request")
+        )
+      },
+      promo => {
+        promoRepository.update(promo.id, Promo(promo.id, promo.product, promo.looseAmount)).map { _ =>
+          Redirect(routes.PromoController.updatePromoForm(promo.id)).flashing("success" -> "promo updated")
+        }
+      }
+    )
+  }
+
+
   def addPromoHandle = Action.async { implicit  request =>
     var prod:Seq[Product] = Seq[Product]()
     productRepository.list().onComplete{
@@ -56,11 +92,21 @@ class PromoController @Inject()(cc: MessagesControllerComponents, promoRepositor
     )
   }
 
+  def getPromos: Action[AnyContent] = Action.async { implicit request =>
+    val items = promoRepository.list()
+    items.map( i => Ok(views.html.promos(i)))
+  }
+
   def get(id: Long) = Action.async {
     val result = promoRepository.getById(id)
     result map {r =>
       Ok(Json.toJson(r));
     }
+  }
+
+  def deletePromo(id: Long): Action[AnyContent] = Action { implicit request =>
+    promoRepository.delete(id)
+    Redirect(routes.PromoController.getPromos())
   }
 
   def update() = Action.async { request =>
