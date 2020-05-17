d359f924ee65797916ab57df78847bbce9d2cb5b
SzymonSmykala <ctk7000@gmail.com>
2020-05-17 21:00:58 +0200

    display category name in ProductsView.js

--- client/src/views/ProductsView.js
+++ client/src/views/ProductsView.js
@@ -2,35 +2,45 @@ import * as React from "react";
 import {Component} from "react";
 import {ProductService} from "../services/ProductService";
 import {Table} from "reactstrap";
+import {CategoryService} from "../services/CategoryService";
 
 
 export class ProductsView extends Component {
 
     productService: ProductService;
+    categoryService: CategoryService;
 
     constructor() {
         super();
         this.productService = new ProductService();
-        this.state = {products: []};
+        this.categoryService = new CategoryService();
+        this.state = {products: [], categories: new Map(), fetched: false};
     }
 
     async componentDidMount(): void {
         let fetched = await this.productService.fetchAll();
         this.setState({products: fetched});
+        let categories  = await this.categoryService.fetchAll();
+        categories.map(c => (this.state.categories.set(c.id, c.name)));
+        this.setState({fetched:true})
+
     }
 
     render() {
+        if (this.state.fetched === false) return null;
+
         const tableHeader =  <tr>
             <th>Id</th>
             <th>Name</th>
             <th>CategoryId</th>
         </tr>
 
+        console.log("Category: " + this.state.categories.get(6));
         let tableObjects = this.state.products.map(p => (
             <tr>
                 <th>{p.id}</th>
                 <th>{p.name}</th>
-                <th>{p.categoryId}</th>
+                <th>{this.state.categories.get(p.categoryId)}</th>
             </tr>
 
         ));
